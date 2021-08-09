3350f05708d12d924c49df55bf788b7b983ad896
Utkarsh Agarwal <71901880+utkarshagarwal97@users.noreply.github.com>
2021-08-10 00:22:01 +0530

    Update Specific Character Count.py

--- Specific Character Count.py
+++ Specific Character Count.py
@@ -1,3 +1,3 @@
-a = input ("Enter your text: ")
-b = input ("Enter the alphabet to find occurance for: ")
+a = input ("Enter your text: ") #enter your complete text
+b = input ("Enter the alphabet to find occurance for: ") #enter the alphabet you want occurance for
 print (a.count(b))
